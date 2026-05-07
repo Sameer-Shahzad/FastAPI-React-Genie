156e205bb926db58f6825fbe59cb53757754233e
Sameer Shahzad <sameershahzad835@gmail.com>
2026-05-07 22:43:37 +0500

    Config in main.py

--- backend/main.py
+++ backend/main.py
@@ -2,5 +2,22 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
-    
+    title = "Choose your own Adventure Game API",
+    description = "API to generate cool stories",
+    version = "0.1.0",
+    docs_url = "/docs",
+    redoc_url = "/redoc",
 )
+
+app.add_middleware (
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods = ["*"],
+    allow_credentials = True,
+    allow_headers = ["*"],
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run( "main:app", host = "0.0.0.0", port = 8000, reload = True )
+    
